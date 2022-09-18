155c22258239e008dde3211dbab81b78c9b80869
the-RIX <mc.therash@gmail.com>
2022-09-18 22:20:00 +0200

    blackjacklib.h first custom datatypes

---
+++ blackjacklib.h
@@ -0,0 +1,24 @@
+//hand
+
+//stack
+
+//table
+
+const int NUMBER_OF_CARDS = 312;
+
+
+enum Color {Diamonds, Hearts, Spades, club};
+enum Face {A, two, three, four, five, six, seven, eight, nine, ten, J, Q, K};
+
+typedef struct Card {
+    Color color;
+    Face face;
+} card;
+
+typedef struct Stack {
+    card cards[NUMBER_OF_CARDS];
+} stack_t;
+
+typedef struct Table {
+    stack_t Stack;
+} table_t;
