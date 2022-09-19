e52018ecf762b864c03386459ff8cbd878f00793
finnboett <finnbottche8@gmail.com>
2022-09-19 06:09:46 +0200

    updatet code upload

--- blackjacklib.c
+++ blackjacklib.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "./blackjacklib.h"
+#include <time.h>
 
 
 deck_t *generateDeck() {
@@ -43,15 +44,59 @@ void main(){
   for (int i = 0; i <= 311; i++){
 
     //printf(deck1->Cards[1].color, deck1->Cards[1].face);
-    printf("%i: %i, %i\n", i, stack1->Cards[i].color, stack1->Cards[i].face);
+    //printf("%i: %i, %i\n", i, stack1->Cards[i].color, stack1->Cards[i].face);
 
   }
-
+  handOpenCardPlayer(NULL);
+  handOpenCardDealer(NULL);
 }
 
-void handOpenCardPlayer(){
+void handOpenCardPlayer(table_t *table){
+  srand(time(NULL));
   int card = rand() % 311;
-  printf(card)
+  printf("0\n");
+  while(table->stack->Cards[card].face == -1){
+    srand(time(NULL));
+    int card = rand() % 311;
+    printf("1\n");
+  }
+
+  for(int i=0; i<21; i++){
+    if(table->player->cards[i].face == -1){
+      printf("2\n");
+      table->player->cards[i].face = table->stack->Cards[card].face;
+      table->player->cards[i].color = table->stack->Cards[card].color;
+      printf("3\n");
+      table->stack->Cards[card].face = -1;
+      table->stack->Cards[card].color = -1;
+      printf("4\n");
+      break;
+    }
+  }
+}
+
+void handOpenCardDealer(table_t *table){
+  srand(time(NULL));
+  int card = rand() % 311;
+  printf("0\n");
+  while(table->stack->Cards[card].face == -1){
+    srand(time(NULL));
+    int card = rand() % 311;
+    printf("1\n");
+  }
+
+  for(int i=0; i<21; i++){
+    if(table->dealer->cards[i].face == -1){
+      printf("2\n");
+      table->dealer->cards[i].face = table->stack->Cards[card].face;
+      table->dealer->cards[i].color = table->stack->Cards[card].color;
+      printf("3\n");
+      table->stack->Cards[card].face = -1;
+      table->stack->Cards[card].color = -1;
+      printf("4\n");
+      break;
+    }
+  }
 }
 
 table_t *generateTable(stack_t *stack) {
@@ -61,7 +106,7 @@ table_t *generateTable(stack_t *stack) {
     hand_t *playerHand = malloc(21*sizeof(card));
     for (int i=0; i<21; i++) {
         dealerHand->cards[i].color=-1;
-        dealerHand->cards[i].color=-1;
+        dealerHand->cards[i].face=-1;
         playerHand->cards[i].color=-1;
         playerHand->cards[i].face=-1;
     }
@@ -82,7 +127,7 @@ void drawTable(table_t *table) {
         printf("%i", table->player->cards[i].color);
     }
     for (int i=0; i<21; i++) {
-        printf("%i", table->player->cards[i].face);      
+        printf("%i", table->player->cards[i].face);
     }
 }
 
